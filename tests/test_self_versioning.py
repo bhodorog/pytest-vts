4f87ed3af97fece89dccdced32e38c4c60d4ffeb
Bogdan Hodorog <bogdan.hodorog@domain.com.au>
2016-07-04 10:01:43 +1000

    Bump version. Self versioning test doesn't fail on tag.

--- tests/test_self_versioning.py
+++ tests/test_self_versioning.py
@@ -8,17 +8,29 @@ import pytest_vts
 
 
 @pytest.fixture
-def last_prod_tag():
-    cmd = shlex.split("git describe --tags --abbrev=0 --match='v*'")
+def git_describe():
+    cmd = shlex.split("git describe --tags --long --match='v*'")
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
     out, err = proc.communicate()
-    return out.decode().strip("\n")
+    described = out.decode().strip("\n")
+    reversed_described = described[::-1]
+    rev_gitref, rev_no_of_commits, rev_tag = reversed_described.split("-", 2)
+    tag = rev_tag[::-1]
+    no_of_commits = rev_no_of_commits[::-1]
+    return tag, no_of_commits
 
 
-def test_version_needs_bumping(last_prod_tag):
+def test_version_needs_bumping(git_describe):
+    last_prod_tag, no_of_commits = git_describe
     pv = pkg_resources.parse_version
-    err_msg = ("Bump the version following PEP440 rules")
-    assert pv(pytest_vts.__version__) > pv(last_prod_tag), err_msg
+    err_msg_prefix = "Bump the version following PEP440 rules"
+    err_msg = ("{}: bondi.__version__({}) should be > than last prod tag "
+               "({})").format(err_msg_prefix,
+                              pytest_vts.__version__, last_prod_tag)
 
+    version_greater_than_tag = pv(pytest_vts.__version__) > pv(last_prod_tag)
+    no_newer_commits_over_tag = no_of_commits == "0"
+    assert (version_greater_than_tag or no_newer_commits_over_tag,
+            "{}: {}".format(err_msg, ""))
 
